b4320819125e74ce6bc93406fbd6d334454e51e1
Augustin Ribreau <augustin.ribreau@gmail.com>
2020-12-26 14:53:48 +0100

    fix: add 1 month 🤧

--- src/js/date.js
+++ src/js/date.js
@@ -2,7 +2,7 @@ const date = document.getElementById("date");
 const time = document.getElementById("time");
 const now = new Date();
 const nowFormat =
-  now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate();
+  now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
 const timeNowFormat = now.getHours() + ":" + now.getMinutes();
 date.value = nowFormat;
 time.value = timeNowFormat;
