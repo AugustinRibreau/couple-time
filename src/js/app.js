4d26e6fe803466efdb6f901fa89af93cad00252e
Augustin Ribreau <augustin.ribreau@gmail.com>
2021-01-14 19:20:23 +0100

    feat: calcul and design display info

---
+++ src/js/app.js
@@ -0,0 +1,50 @@
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+const now = new Date();
+const content = document.getElementById("content");
+const love = document.getElementById("love");
+const main = document.getElementById("main");
+const nowDateYMD =
+  now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
+
+// Error when minute is 1 or 2, for fix add 0 before, like 01 or 02.
+now.getMinutes() < 10
+  ? (time.value = now.getHours() + ":0" + now.getMinutes())
+  : (time.value = now.getHours() + ":" + now.getMinutes());
+date.value = nowDateYMD;
+
+love.addEventListener("click", function () {
+  let userDate = date.value.split("-");
+  let userTime = time.value.split(":");
+
+  let userValue = new Date(
+    userDate[0],
+    userDate[1] - 1,
+    userDate[2],
+    userTime[0],
+    userTime[1]
+  );
+  // No date, no continue
+
+  if(userDate[0] === "" || userTime[0] === ""){
+    return;
+  }
+  content.style.animation = "wave 1s";
+  setTimeout(() => {
+    setDataLove(userValue);
+    document.getElementById("content_body").classList.remove("d-none")
+  }, 1500);
+});
+
+function setDataLove(userDate) {
+  const now = new Date();
+  document.getElementById("differenceYear").innerHTML = changeStatusNumber(now.getFullYear() - userDate.getFullYear());
+  document.getElementById("differenceMonth").innerHTML = changeStatusNumber((now.getMonth() + 1) - (userDate.getMonth() + 1));
+  document.getElementById("differenceDate").innerHTML = changeStatusNumber((now.getDate()) - (userDate.getDate()));
+  document.getElementById("differenceHours").innerHTML = changeStatusNumber((now.getHours()) - (userDate.getHours()));
+  document.getElementById("differenceMinutes").innerHTML = changeStatusNumber((now.getMinutes()) - (userDate.getMinutes()));
+}
+
+function changeStatusNumber(number){
+  return (number < 0) ? number * -1 : number;
+}
--- src/js/date.js
+++
@@ -1,8 +0,0 @@
-const date = document.getElementById("date");
-const time = document.getElementById("time");
-const now = new Date();
-const nowFormat =
-  now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
-const timeNowFormat = now.getHours() + ":" + now.getMinutes();
-date.value = nowFormat;
-time.value = timeNowFormat;
